36212d60975a3712dbfb8bf6ac28fe33d58a4eaf
Tsarcasm831 <88747937+Tsarcasm831@users.noreply.github.com>
2025-08-16 13:17:44 -0600

    Refactor user defaults into modular data

--- map/defaults/model-default.js
+++ map/defaults/model-default.js
@@ -3,12 +3,16 @@ import { DEFAULT_ROADS } from './parts/roads.js';
 import { DEFAULT_POI } from './parts/poi.js';
 import { DEFAULT_WALLS } from './parts/walls.js';
 import { DEFAULT_RIVERS } from './parts/rivers.js';
+import { DEFAULT_TERRAIN } from './parts/terrain.js';
 
 export const DEFAULT_MODEL = {
   districts: DEFAULT_DISTRICTS,
   roads: DEFAULT_ROADS,
   poi: DEFAULT_POI,
   walls: DEFAULT_WALLS,
-  rivers: DEFAULT_RIVERS
+  rivers: DEFAULT_RIVERS,
+  grass: DEFAULT_TERRAIN.grass,
+  forest: DEFAULT_TERRAIN.forest,
+  mountains: DEFAULT_TERRAIN.mountains
 };
 
--- map/defaults/parts/walls.js
+++ map/defaults/parts/walls.js
@@ -1,4 +1,11 @@
 export const DEFAULT_WALLS = [
-  {"id":"wall-1","name":"","desc":"","cx":52.78,"cy":51.04,"r":35.609685480217316,"width":8}
+  {
+    "id": "wall-1",
+    "name": "",
+    "desc": "",
+    "cx": 52.78,
+    "cy": 51.04,
+    "r": 35.609685480217316,
+    "width": 8
+  }
 ];
-
